276b2778bac755510e72e897c1a2fde8112c116b
Adam <casselldev@gmail.com>
2020-09-24 19:52:11 -0700

    debugging static

--- cybex/settings.py
+++ cybex/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = 'static'
+STATIC_ROOT = 'static/cybexweb'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "cybexweb/graph/build/static"),
