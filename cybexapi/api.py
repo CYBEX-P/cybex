2f8310a14c73d318244a9be5cfcb990b5cc21356
skasero <spencerkkr@gmail.com>
2020-10-28 09:56:55 -0700

    Able to import nodes, no relationship yet

--- cybexapi/api.py
+++ cybexapi/api.py
@@ -18,7 +18,7 @@ from cybexapi.whoisXML import whois, insertWhois
 from cybexapi.enrichments import insert_domain_and_user, insert_netblock, insert_domain, resolveHost, getNameservers, getRegistrar, getMailServer
 from cybexapi.cybexlib import cybexCountHandler, cybexRelatedHandler, pull_ip_src
 from cybexapi.shodanSearch import shodan_lookup, insert_ports
-from cybexapi.import_json import import_json
+from cybexapi.import_json import import_json, writeToDB
 import json
 from cybexapi.wipe_db import wipeDB
 import pandas as pd
@@ -121,6 +121,7 @@ class exportNeoDB(APIView):
         graph = connect2graph(current_user.graphdb.dbuser, current_user.graphdb.dbpass,
                               current_user.graphdb.dbip, current_user.graphdb.dbport)
         g = export(graph)
+        # return Response(g)
         # print(g)
         p = processExport(g)
         # print(p)
@@ -405,7 +406,11 @@ class importJson(APIView):
     authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
 
     def post(self, request, format=None):
-        responce = Response(import_json(request.data))
+        # Testing below
+        current_user = request.user
+        graph = connect2graph(current_user.graphdb.dbuser, current_user.graphdb.dbpass,
+                              current_user.graphdb.dbip, current_user.graphdb.dbport)
+        responce = Response(import_json(graph,request.data))
         return(responce)
 
 
