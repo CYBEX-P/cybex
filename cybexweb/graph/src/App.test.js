28b007214083d56dff42a2854684076e0b97f1d7
Adam <casselldev@gmail.com>
2021-03-03 22:28:34 -0800

    Removed unnecessary and deprecated files
    Also added some mild commenting and removed some deprecated code

--- cybexweb/graph/src/App.test.js
+++ cybexweb/graph/src/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Place react tests here...
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
 });
